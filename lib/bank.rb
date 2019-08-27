16da42e67bca2356ff6d9ee307d128f7a3a68220
Kaja <catcalledkaja@gmail.com>
2019-08-27 16:34:06 +0100

    adding error if not enough money

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,8 +1,17 @@
 
 class Bank
+attr_reader :balance
+
+  def initialize
+    @balance = 0
+  end
 
   def debit(money)
 
-  end 
+  end
 
+  def credit(money)
+    raise "Not enough money" if money > @balance
+  end
+  
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -7,4 +7,15 @@ describe Bank do
     expect(bank).to respond_to(:debit).with(1).argument
   end
 
+  it 'reponds to credit with one argument' do
+    expect(bank).to respond_to(:credit)
+  end
+
+  it ' startst with zero balance' do
+    expect(bank.balance).to eq 0
+  end
+
+  it 'doest not allow to withdraw money if balance is below 0 ' do
+    expect{ bank.credit(10) }.to raise_error "Not enough money"
+  end
 end
