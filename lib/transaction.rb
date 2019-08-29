7860d79d1474dc9665ce478a854cf1bd370db8f2
Kaja <catcalledkaja@gmail.com>
2019-08-29 09:52:18 +0100

    adding a record of transaction

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,14 +1,12 @@
 
 class Transaction
 
-  attr_reader :date, :history
+  attr_reader :record
 
   def initialize
-    @history = []
+    @record= {}
   end
 
-  def information(cash_in, cash_out, balance)
-     @history.push(cash_in.to_f, cash_out.to_f, balance.to_f)
-  end
+
 
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -4,17 +4,9 @@ describe Transaction do
 
 subject(:transaction) { described_class.new }
 
-  it 'starts with an empty history' do
-    expect(transaction.history).to eq([])
-  end
+  it 'should make a record of transaction' do
+    expect(transaction.record).to eq({})
 
-  # it 'has a date of the transaction' do
-  #   date = Time.new.strftime("%d/%m/%Y")
-  #   expect(transaction.date).to eq("#{date}")
-  # end
-
-  it 'can take a note of cash in, cash, out, balance and date' do
-    expect(transaction.information("20","20","0")).to eq([20.00,20.00,0.00])
   end
 
 end
