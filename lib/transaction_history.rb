c1c0baa19ad85b3bce1c21760550b3b92e3318ad
Kaja <catcalledkaja@gmail.com>
2019-08-30 11:03:34 +0100

    initializing empty statment on transaction_history

--- lib/transaction_history.rb
+++ lib/transaction_history.rb
@@ -1,10 +1,11 @@
 
 class Transaction_history
 
-attr_reader :date
+attr_reader :date, :statement
 
   def initialize
     @date = Time.new.strftime("%d/%m/%Y")
+    @statement = []
   end
 
 end
--- spec/transaction_history_spec.rb
+++ spec/transaction_history_spec.rb
@@ -3,11 +3,15 @@ require 'transaction_history'
 describe Transaction_history do
 
 subject(:transaction_history) { described_class.new }
-  context '#date' do
-    it 'has a date of the transaction' do
-      date = Time.new.strftime("%d/%m/%Y")
-      expect(transaction_history.date).to eq("#{date}")
+    context '#initialize' do
+      it 'has a date of the transaction' do
+        date = Time.new.strftime("%d/%m/%Y")
+        expect(transaction_history.date).to eq("#{date}")
+      end
+      it 'comes with an empty statment ' do
+        expect(transaction_history.statement).to eq([])
+      end
     end
-  end
-  
+
+
 end
